dfb920383226814a09599369d2e2a61dbc8e803b
Gabriel Pessighello <gabriel.pessighello@indico.net.br>
2021-04-24 01:04:47 -0300

    feat: implement layout with widgets
    CHANGE LOG:
    - Material Design components
    - Widget refactoring
    - Code abstraction to reuse

--- bytebank/lib/main.dart
+++ bytebank/lib/main.dart
@@ -1,6 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(Text(
-      "Hello, world",
-      textDirection: TextDirection.ltr,
-    ));
+void main() => runApp(
+      MaterialApp(
+        home: Scaffold(
+          body: ListTransfer(),
+          appBar: AppBar(
+            title: Text('Transfers'),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+          ),
+        ),
+      ),
+    );
+
+class ListTransfer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransfer(Transfer(100.00, 12345)),
+        ItemTransfer(Transfer(200, 98433)),
+        ItemTransfer(Transfer(200, 98433)),
+      ],
+    );
+  }
+}
+
+class ItemTransfer extends StatelessWidget {
+  final Transfer _transfer;
+  ItemTransfer(this._transfer);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(this._transfer.value.toString()),
+        subtitle: Text(this._transfer.accountNumber.toString()),
+      ),
+    );
+  }
+}
+
+class Transfer {
+  final double value;
+  final int accountNumber;
+
+  Transfer(this.value, this.accountNumber);
+}
